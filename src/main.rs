f819a827b21c745b54e73f99d7c33114dfbb21aa
Gabriel Antunes <gabriel.tantunes@b2wdigital.com>
2022-04-11 18:47:31 -0300

    feat: guessing game in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number: i32 = rand::thread_rng().gen_range(1..101);
+
+    println!("Guess the number!");
+
+    loop {
+        println!("Please, input your guess:");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("You guessed: {}", guess);
+
+        if guess.trim().cmp(&"quit".to_string()) == Ordering::Equal {
+            println!("A see you later");
+            break;
+        }
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
